390743f6af6b1e66dca67cfdf807d667054ee2b5
Misozi Lyatumba <157115229+Mimiachi-Techbabe@users.noreply.github.com>
2024-11-29 16:17:02 +0200

    Add files via upload

---
+++ localstore.php
@@ -0,0 +1,60 @@
+<?php
+
+session_start();
+
+include("includes/db.php");
+include("includes/header.php");
+include("functions/functions.php");
+include("includes/main.php");
+
+?>
+  <!-- MAIN -->
+  <main>
+    <!-- HERO -->
+    <div class="nero">
+      <div class="nero__heading">
+        <span class="nero__bold">Local </span>Stores
+      </div>
+      <p class="nero__text">
+      </p>
+    </div>
+  </main>
+
+
+<div id="content" ><!-- content Starts -->
+
+<div class="container-fluid" ><!-- container Starts -->
+
+
+<p>-We only have one office for all queries Head Office; Town Centre.Cha-Cha Road, Lusaka</p>
+<p>-Trading should be done online, however plastic waste products can be shipped directly to buyers or by picking them up from our offices if need be</p>
+<p>-Honestly and Intergrity must be adhered to at time of collection of goods</p>
+
+
+<div class="col-md-12" ><!-- col-md-12 Starts -->
+
+
+
+</div><!-- services row Ends -->
+
+</div><!-- col-md-12 Ends -->
+
+
+
+</div><!-- container Ends -->
+</div><!-- content Ends -->
+
+
+
+<?php
+
+include("includes/footer.php");
+
+?>
+
+<script src="js/jquery.min.js"> </script>
+
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
